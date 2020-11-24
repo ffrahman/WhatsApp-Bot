5a1ba742aa7bf11c2caaa1836c7150bc2e0e720f
Fatur Rahman <50137981+faturrahman1129@users.noreply.github.com>
2020-11-24 21:32:26 +0700

    Add files via upload

---
+++ WhatsApp.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+driver.get('https://web.whatsapp.com/')
+
+name = input('Nama: ')
+msg = input('Pesan: ')
+count = int(input('Jumlah: '))
+
+input("Enter")
+
+
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+
+msg_box = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]')
+
+for i in range(count):
+    msg_box.send_keys(msg)
+    button = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button')
+    button.click()
+
